20413d817c4c1dc29295207e21a93337c560e0b9
ddx <xiededong001211@gmail.com>
2021-09-27 19:51:16 -0400

    Added two files Raptor.java and RaptorTest.java as class files where Raptor implements Tradable and Drivable interfaces. Added one instance of Raptor to all_items list in Trader.java

---
+++ src/main/java/Raptor.java
@@ -0,0 +1,55 @@
+public class Raptor implements Tradable, Drivable {
+    private int maxSpeed;
+    private int price;
+
+    /**
+     * Constructor of the class
+     */
+    public Raptor() {
+        this.maxSpeed = 1;
+        this.price = 1000000000;
+    }
+
+    /**
+     * Get the price of a Raptor
+     * @return the price of the Raptor instance
+     */
+    public int getPrice() { return this.price;}
+
+    /**
+     * Increase the price by 10 million
+     */
+    public void increasePrice() { this.price += 10000000; }
+
+    /**
+     * Decrease the price by 10 million
+     */
+    public void decreasePrice () { this.price -= 10000000; }
+
+    /**
+     * Increase the maximum speed of this Raptor.
+     **/
+    public void upgradeSpeed() {
+        if (this.maxSpeed < 299792358) {
+            this.maxSpeed += 100;
+        }
+    }
+
+    /**
+     * Decrease the maximum speed of this Raptor.
+     **/
+    public void downgradeSpeed() {
+        if (this.maxSpeed > 100) {
+            this.maxSpeed -= 100;
+        }
+    }
+
+
+    /**
+     * Return the maximum speed of this Raptor.
+     *
+     * @return    The current maximum speed of this Raptor.
+     **/
+    public int getMaxSpeed() { return this.maxSpeed; }
+
+}
